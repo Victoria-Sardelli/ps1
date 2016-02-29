0ca5e7e4e90ad39a711aadb0f42fb2326780e3c5
Mike Nolan <me@nolski.rocks>
2016-02-28 22:08:57 -0500

    created v2na

--- ps1.py
+++ ps1.py
@@ -17,66 +17,59 @@ import string
 
 def checkpalindrome(s):
     """
-    lead writer: 
+    lead writer:
         Victoria Sardelli
 
     what it does:
         Takes a string as input and prints out whether or not it's a palindrome
         Disregards capitalization, spaces, and punctuation
-    
+
     Expected arguments:
-        s - string    
+        s - string
     """
-    
+
     # get string and strip whitespace
     s1 = s.replace(" ","")
-    
+
     # get rid of punctuation
     exclude = set(string.punctuation)
     s1 = "".join(ch for ch in s1 if ch not in exclude)
-    
+
     # capitalize all letters in string
     s1 = s1.upper()
 
-    # split string into two halves for comparison    
+    # split string into two halves for comparison
     half = len(s1)//2
-    if len(s1) % 2 == 0:    
+    if len(s1) % 2 == 0:
         sLeft = s1[:half]
     else:
         sLeft = s1[:half+1]
     sRight = s1[half:]
     sRight = sRight[::-1] #flip it around
-    
+
     if sLeft == sRight:
         print("\"" + s + "\" is a palindrome!\n")
     else:
         print("\"" + s + "\" is not a palindrome.\n")
-        
-        
+
+
 def v2na(verb):
     """
-    lead writer:
-    
-
+    lead writer: Mike Nolan
     what it does:
-    
-    
-    Expected arguments:
-    
+    Expected arguments: verb: string
     """
-
+    root = verb[:-3] # cuts off 'ity'
+    if root[-2] == 'i':
+        root = root + 'e'
+    print(root)
 
 def checkword(s):
     """
     lead writer:
-    
-
     what it does:
-    
-    
     Expected arguments:
-    
     """
 
-    
-    
+
+
